b06b4e9549b8451f21884be1367e867e24d4d4ae
Aaron Thompson <aaron@standardcyborg.com>
2019-08-18 19:32:59 -0700

    StandardCyborgUI: expose Bundle.scuiBundle as a public method

--- StandardCyborgUI/StandardCyborgUI/Bundle+SCUI.swift
+++ StandardCyborgUI/StandardCyborgUI/Bundle+SCUI.swift
@@ -3,5 +3,5 @@ import Foundation
 extension Bundle {
     // Any entity using this prop should exist in the same target as the Bundle(for:) argument otherwise its contents
     // won't be packaged correctly.
-    static let scuiBundle = Bundle(for: ShutterButton.self)
+    public static let scuiBundle = Bundle(for: ShutterButton.self)
 }
