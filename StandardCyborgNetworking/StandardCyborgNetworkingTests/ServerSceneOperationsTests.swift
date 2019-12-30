6353f59dad2e0db79186907ec24cb2100fa7641b
Aaron Thompson <aaron@standardcyborg.com>
2019-12-30 07:59:17 -0800

    Add ServerFetchScenesOperation and ServerDeleteScenesOperation

--- StandardCyborgNetworking/StandardCyborgNetworkingTests/ServerSceneOperationsTests.swift
+++ StandardCyborgNetworking/StandardCyborgNetworkingTests/ServerSceneOperationsTests.swift
@@ -11,14 +11,12 @@ class ServerSceneOperationsTests: XCTestCase {
     let apiClient = TestAPIClient()
     let dataSource = TestDataSource()
     
-    // override func setUp() {}
-    
     func testServerAddSceneOperation() {
         let expect = expectation(description: "ServerAddSceneOperation")
         
-        apiClient.setResponse(for: "scenes", jsonPath: _fullTestFixturePath("scenes-success.json"))
-        apiClient.setResponse(for: "scenes/8UHPFfY7/versions/1", jsonPath: _fullTestFixturePath("scenes-uid-versions-success.json"))
-        apiClient.setResponse(for: "direct_uploads", jsonPath: _fullTestFixturePath("direct_uploads-success.json"))
+        apiClient.setResponse(for: "scenes", method: .POST, jsonPath: _fullTestFixturePath("scenes-post-success.json"))
+        apiClient.setResponse(for: "scenes/8UHPFfY7/versions/1", method: .PUT, jsonPath: _fullTestFixturePath("scenes-uid-versions-put-success.json"))
+        apiClient.setResponse(for: "direct_uploads", method: .POST, jsonPath: _fullTestFixturePath("direct_uploads-post-success.json"))
         
         let gltfPath = _fullTestFixturePath("empty.gltf")
         let thumbnailPath = _fullTestFixturePath("empty.jpeg")
@@ -45,43 +43,55 @@ class ServerSceneOperationsTests: XCTestCase {
         wait(for: [expect], timeout: 5)
     }
     
-    func testServerSignInOperation() {
-        let successExpect = expectation(description: "ServerSignInOperation-success")
-        let failureExpect = expectation(description: "ServerSignInOperation-failure")
+    func testServerFetchScenesOperation() {
+        let expect = expectation(description: "ServerFetchScenesOperation")
         
-        apiClient.setResponse(for: "auth/sign_in", jsonPath: _fullTestFixturePath("auth-sign_in-success.json"))
+        apiClient.setResponse(for: "scenes", method: .GET, jsonPath: _fullTestFixturePath("scenes-get-success.json"))
         
-        ServerSignInOperation(dataSource: dataSource, apiClient: apiClient, email: "test@example.com", password: "password")
+        ServerFetchScenesOperation(dataSource: dataSource,
+                                   serverAPIClient: apiClient)
         .perform { result in
             switch result {
-            case let .success(scene):
-                XCTAssertEqual(scene.email, "test@example.com")
-                XCTAssertEqual(scene.name, "John Doe")
-                XCTAssertEqual(scene.key, "siuayACV")
-                XCTAssertEqual(scene.teams?.first?.name, "John Doe")
-                XCTAssertEqual(scene.teams?.first?.personal, true)
-                XCTAssertEqual(scene.teams?.first?.uid, "uMQrVFHx")
+            case let .success(scenes):
+                XCTAssertEqual(scenes.count, 2)
+                
+                XCTAssertEqual(scenes.first?.createdAt, DateTimeTransform.fromString("2019-12-29T15:41:29.783Z"))
+                XCTAssertEqual(scenes.first?.key, "bGaWFVR7")
+                XCTAssertEqual(scenes.first?.teamUID, "mWD982Bo")
+                
+                XCTAssertEqual(scenes.first?.versions.count, 1)
+                XCTAssertEqual(scenes.first?.versions.first?.versionNumber, 1)
+                XCTAssertEqual(scenes.first?.versions.first?.key, "UWcv2KmW")
+                XCTAssertEqual(scenes.first?.versions.first?.sceneUID, "bGaWFVR7")
             case let .failure(error):
                 XCTFail("Failure: \(error)")
             }
-            successExpect.fulfill()
+            
+            expect.fulfill()
         }
-        wait(for: [successExpect], timeout: 5)
         
-        apiClient.setResponse(for: "auth/sign_in", jsonPath: _fullTestFixturePath("auth-sign_in-failure.json"))
+        wait(for: [expect], timeout: 5)
+    }
+    
+    func testServerDeleteSceneOperation() {
+        let expect = expectation(description: "ServerFetchScenesOperation")
         
-        ServerSignInOperation(dataSource: dataSource, apiClient: apiClient, email: "test@example.com", password: "password")
-        .perform { result in
-            switch result {
-            case let .success(scene):
-                XCTFail("Expected failure but got scene \(scene)")
-            case let .failure(error):
-                XCTAssertTrue(error is ServerOperationError)
-                XCTAssertEqual(error as! ServerOperationError, ServerOperationError.genericErrorString("Invalid login credentials. Please try again."))
-            }
-            failureExpect.fulfill()
+        apiClient.setResponse(for: "scenes/abc123", method: .DELETE, jsonPath: _fullTestFixturePath("scenes-uid-delete-success.json"))
+        
+        let scene = ServerScene(createdAt: Date(),
+                                key: "abc123",
+                                teamUID: "def456",
+                                versions: [])
+        
+        ServerDeleteSceneOperation(scene: scene,
+                                   dataSource: dataSource,
+                                   serverAPIClient: apiClient)
+        .perform { error in
+            XCTAssertNil(error)
+            expect.fulfill()
         }
-        wait(for: [failureExpect], timeout: 5)
+        
+        wait(for: [expect], timeout: 5)
     }
     
     private func _fullTestFixturePath(_ relativePath: String) -> String {
