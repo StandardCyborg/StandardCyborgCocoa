0735898bc8b89b8342fd516886fe22c0cef20872
Ricky Reusser <ricky@standardcyborg.com>
2020-02-20 11:10:04 -0800

    StandardCyborgFusion: Adds umbrella headers

---
+++ StandardCyborgFusion/osx/StandardCyborgFusion.framework/Versions/A/CppHeaders/StandardCyborgData/StandardCyborgData.hpp
@@ -0,0 +1,39 @@
+//
+//  StandardCyborgData.hpp
+//  StandardCyborgSDK
+//
+//  Created by Ricky Reusser on 2/20/20.
+//  Copyright © 2020 Standard Cyborg. All rights reserved.
+//
+
+#pragma once
+
+#include <StandardCyborgData/AssertHelper.hpp>
+#include <StandardCyborgData/BoundingBox3.hpp>
+#include <StandardCyborgData/ColorImage.hpp>
+#include <StandardCyborgData/DebugHelpers.hpp>
+#include <StandardCyborgData/DepthImage.hpp>
+#include <StandardCyborgData/Face3.hpp>
+#include <StandardCyborgData/Geometry.hpp>
+#include <StandardCyborgData/Labels.hpp>
+#include <StandardCyborgData/Landmark.hpp>
+#include <StandardCyborgData/Line.hpp>
+#include <StandardCyborgData/Mat3x3.hpp>
+#include <StandardCyborgData/Mat3x4.hpp>
+#include <StandardCyborgData/Mat4x4.hpp>
+#include <StandardCyborgData/MathHelpers.hpp>
+#include <StandardCyborgData/MeshTopology.hpp>
+#include <StandardCyborgData/PerspectiveCamera.hpp>
+#include <StandardCyborgData/Plane.hpp>
+#include <StandardCyborgData/Point2D.hpp>
+#include <StandardCyborgData/Polyline.hpp>
+#include <StandardCyborgData/Quaternion.hpp>
+#include <StandardCyborgData/Rect2D.hpp>
+#include <StandardCyborgData/SceneGraph.hpp>
+#include <StandardCyborgData/Size2D.hpp>
+#include <StandardCyborgData/StandardCyborgData.hpp>
+#include <StandardCyborgData/Transform.hpp>
+#include <StandardCyborgData/Vec2.hpp>
+#include <StandardCyborgData/Vec3.hpp>
+#include <StandardCyborgData/Vec4.hpp>
+#include <StandardCyborgData/VertexSelection.hpp>
